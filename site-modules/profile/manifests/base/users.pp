f7c5e7779c2a259d44bdad14127f679c541e3f29
Aaron Reisman <aaron@reisman.org>
2022-12-29 00:18:49 -0600

    Don't manage the user's home

--- site-modules/profile/manifests/base/users.pp
+++ site-modules/profile/manifests/base/users.pp
@@ -2,7 +2,6 @@ class profile::base::users {
   @user { 'provisioned-user':
     ensure     => 'present',
     name       => $facts['provisioned_user'],
-    managehome => true,
     membership => 'minimum',
     password   => '',
   }
