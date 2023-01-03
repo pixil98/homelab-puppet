0343187b8a23f26a81e3bd75f6c0fcd110c8e24f
Aaron Reisman <aaron@reisman.org>
2023-01-02 23:04:38 -0600

    Add artifactory repo and install artifactory

--- site-modules/profile/manifests/artifactory.pp
+++ site-modules/profile/manifests/artifactory.pp
@@ -1,7 +1,15 @@
 class profile::artifactory {
-  package { 'artifactory':
-    ensure   => installed,
-    provider => dpkg,
-    source   => "https://releases.jfrog.io/artifactory/artifactory-debs/pool/jfrog-artifactory-oss/jfrog-artifactory-oss-${facts['os.distro.codename']}.deb",
+  apt::source { 'artifactory':
+    location => 'https://releases.jfrog.io/artifactory/artifactory-debs/',
+    release  => 'buster',  # Bullseye release does not exist yet
+    repos    => 'main',
+    key      => {
+      id     => 'A3D085F542F740BBD7E3A2846B219DCCD7639232',
+      source => 'https://releases.jfrog.io/artifactory/api/gpg/key/public',
+    },
+  }
+
+  package { 'jfrog-artifactory-oss':
+    ensure => present,
   }
 }
