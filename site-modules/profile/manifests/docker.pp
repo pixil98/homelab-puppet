c707bce4e5d57cb14931321ab84e37f46a02c4a1
Aaron Reisman <aaron@reisman.org>
2023-01-07 22:25:20 -0600

    Add insecure-registry

--- site-modules/profile/manifests/docker.pp
+++ site-modules/profile/manifests/docker.pp
@@ -1,6 +1,7 @@
 class profile::docker {
   class { 'docker':
-    registry_mirror => 'http://192.168.1.40',
+    registry_mirror  => 'http://192.168.1.40:5000',
+    extra_parameters => '--insecure-registry 192.168.1.40:5000',
   }
   -> User <| title == provisioned-user |> { groups +> 'docker' }
 }
