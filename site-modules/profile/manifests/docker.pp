b007a15dc5cf0a9c055808631eb191acf7c0614d
Aaron Reisman <aaron@reisman.org>
2023-01-07 21:12:15 -0600

    Add group after docker is installed

--- site-modules/profile/manifests/docker.pp
+++ site-modules/profile/manifests/docker.pp
@@ -2,6 +2,5 @@ class profile::docker {
   class { 'docker':
     registry_mirror => 'http://192.168.1.40',
   }
-
-  User <| title == provisioned-user |> { groups +> 'docker' }
+  -> User <| title == provisioned-user |> { groups +> 'docker' }
 }
