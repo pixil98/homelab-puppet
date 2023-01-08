3e409f6e9332ebe8cf44ddc55454e6dbb384e2fc
Aaron Reisman <aaron@reisman.org>
2023-01-07 21:04:47 -0600

    Add docker mirror

--- site-modules/profile/manifests/docker.pp
+++ site-modules/profile/manifests/docker.pp
@@ -1,4 +1,7 @@
 class profile::docker {
-  class { 'docker': }
-  -> User <| title == provisioned-user |> { groups +> 'docker' }
+  class { 'docker':
+    registry_mirror => 'http://192.168.1.40',
+  }
+
+  User <| title == provisioned-user |> { groups +> 'docker' }
 }
