a84684b27e68d524b50771d0d6d12280eceb7f48
Aaron Reisman <aaron@reisman.org>
2023-01-08 20:03:04 -0600

    Reference registry by dns name

--- site-modules/profile/manifests/docker.pp
+++ site-modules/profile/manifests/docker.pp
@@ -1,7 +1,7 @@
 class profile::docker {
   class { 'docker':
-    registry_mirror  => 'http://192.168.1.40:5000',
-    extra_parameters => '--insecure-registry 192.168.1.40:5000',
+    registry_mirror  => 'http://registry.infrastructure.lab.reisman.org:5000',
+    extra_parameters => '--insecure-registry registry.infrastructure.lab.reisman.org:5000',
   }
   -> User <| title == provisioned-user |> { groups +> 'docker' }
 }
